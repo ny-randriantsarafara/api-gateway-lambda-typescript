6a60a030989299f79805626e26ced642cc2c8ada
randriantsarafara-wbd <165807404+randriantsarafara-wbd@users.noreply.github.com>
2024-07-29 18:54:01 +0200

    chore: created data provider

--- client/admin/src/App.tsx
+++ client/admin/src/App.tsx
@@ -1,10 +1,12 @@
-import {
-  Admin,
-  Resource,
-  ListGuesser,
-  EditGuesser,
-  ShowGuesser,
-} from "react-admin";
+import { Admin, ListGuesser, Resource } from "react-admin";
 import { Layout } from "./Layout";
+import { dataProvider } from "./providers/data-provider.provider";
 
-export const App = () => <Admin layout={Layout}></Admin>;
+export const App = () => (
+  <Admin
+    layout={Layout}
+    dataProvider={dataProvider(import.meta.env.VITE_API_BASE_URL)}
+  >
+    <Resource name="players" list={ListGuesser} />
+  </Admin>
+);
---
+++ client/admin/src/providers/data-provider.provider.ts
@@ -0,0 +1,114 @@
+import {
+  CreateParams,
+  CreateResult,
+  DataProvider,
+  DeleteManyParams,
+  DeleteManyResult,
+  DeleteParams,
+  DeleteResult,
+  fetchUtils,
+  GetListParams,
+  GetListResult,
+  GetManyParams,
+  GetManyReferenceParams,
+  GetManyReferenceResult,
+  GetManyResult,
+  GetOneParams,
+  GetOneResult,
+  Identifier,
+  QueryFunctionContext,
+  RaRecord,
+  UpdateManyParams,
+  UpdateManyResult,
+  UpdateParams,
+  UpdateResult,
+} from "react-admin";
+
+const httpClient = fetchUtils.fetchJson;
+
+export const dataProvider = (baseApiUrl: string): DataProvider => ({
+  async create<RecordType, ResultRecordType extends RaRecord<Identifier>>(
+    resource: string,
+    params: CreateParams
+  ): Promise<CreateResult<ResultRecordType>> {
+    const { json } = await httpClient(`${baseApiUrl}/${resource}`, {
+      method: "POST",
+      body: JSON.stringify(params.data),
+    });
+    return { data: json };
+  },
+  async delete<RecordType extends RaRecord<Identifier>>(
+    resource: string,
+    params: DeleteParams<RecordType>
+  ): Promise<DeleteResult<RecordType>> {
+    const { json } = await httpClient(
+      `${baseApiUrl}/${resource}/${params.id}`,
+      {
+        method: "DELETE",
+      }
+    );
+    return { data: json };
+  },
+  deleteMany<RecordType extends RaRecord<Identifier>>(
+    resource: string,
+    params: DeleteManyParams<RecordType>
+  ): Promise<DeleteManyResult<RecordType>> {
+    throw new Error(`DELETE many ${resource} not implemented`);
+  },
+  async getList<RecordType extends RaRecord<Identifier>>(
+    resource: string,
+    params: GetListParams & QueryFunctionContext
+  ): Promise<GetListResult<RecordType>> {
+    const url = `${baseApiUrl}/${resource}`;
+
+    const { headers, json } = await httpClient(url);
+    return {
+      data: json,
+    };
+  },
+  async getMany<RecordType extends RaRecord<Identifier>>(
+    resource: string,
+    params: GetManyParams<RecordType> & QueryFunctionContext
+  ): Promise<GetManyResult<RecordType>> {
+    const url = `${baseApiUrl}/${resource}`;
+
+    const { headers, json } = await httpClient(url);
+    return {
+      data: json,
+    };
+  },
+  getManyReference<RecordType extends RaRecord<Identifier>>(
+    resource: string,
+    params: GetManyReferenceParams & QueryFunctionContext
+  ): Promise<GetManyReferenceResult<RecordType>> {
+    throw new Error(`GET many reference on ${resource} not implemented`);
+  },
+  async getOne<RecordType extends RaRecord<Identifier>>(
+    resource: string,
+    params: GetOneParams<RecordType> & QueryFunctionContext
+  ): Promise<GetOneResult<RecordType>> {
+    const { json } = await httpClient(`${baseApiUrl}/${resource}/${params.id}`);
+    return {
+      data: json,
+    };
+  },
+  async update<RecordType extends RaRecord<Identifier>>(
+    resource: string,
+    params: UpdateParams
+  ): Promise<UpdateResult<RecordType>> {
+    const { json } = await httpClient(
+      `${baseApiUrl}/${resource}/${params.id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(params.data),
+      }
+    );
+    return { data: json };
+  },
+  updateMany<RecordType extends RaRecord<Identifier>>(
+    resource: string,
+    params: UpdateManyParams
+  ): Promise<UpdateManyResult<RecordType>> {
+    throw new Error(`Update many reference on ${resource} not implemented`);
+  },
+});
