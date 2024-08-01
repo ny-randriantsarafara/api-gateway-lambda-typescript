deca44167169e9db07509cd6ff94cfaae43e9ad3
randriantsarafara-wbd <165807404+randriantsarafara-wbd@users.noreply.github.com>
2024-08-01 11:02:43 +0200

    feat: implemented assignment rest endpoints

--- services/cosfa/src/domain/repositories/assignment.repository.ts
+++ services/cosfa/src/domain/repositories/assignment.repository.ts
@@ -1,14 +1,14 @@
 import { Assignment } from '../entities/assignment.entity';
 
 export type AssignmentFilters = Record<string, any>;
-export type GetCategories = (filters?: AssignmentFilters) => Promise<Assignment[]>;
+export type GetAssignments = (filters?: AssignmentFilters) => Promise<Assignment[]>;
 export type GetAssignmentById = (id: string) => Promise<Assignment | undefined>;
 export type CreateAssignment = (data: Assignment) => Promise<Assignment>;
 export type UpdateAssignment = (id: string, data: Assignment) => Promise<Assignment | undefined>;
 export type DeleteAssignment = (id: string) => Promise<void>;
 
 export type AssignmentRepository = {
-  get: GetCategories;
+  get: GetAssignments;
   create: CreateAssignment;
   getById: GetAssignmentById;
   update: UpdateAssignment;
--- services/cosfa/src/domain/repositories/player.repository.ts
+++ services/cosfa/src/domain/repositories/player.repository.ts
@@ -1,16 +1,16 @@
 import { CreatePlayerDTO, Player } from '../entities/player.entity';
 
 export type PlayerFilters = Record<string, any>;
-export type Get = (filters?: PlayerFilters) => Promise<Player[]>;
-export type GetById = (id: string) => Promise<Player>;
-export type Create = (data: CreatePlayerDTO) => Promise<Player>;
-export type Update = (id: string, data: CreatePlayerDTO) => Promise<Player>;
-export type Delete = (id: string) => Promise<void>;
+export type GetPlayers = (filters?: PlayerFilters) => Promise<Player[]>;
+export type GetPlayerById = (id: string) => Promise<Player>;
+export type CreatePlayer = (data: CreatePlayerDTO) => Promise<Player>;
+export type UpdatePlayer = (id: string, data: CreatePlayerDTO) => Promise<Player>;
+export type DeletePlayer = (id: string) => Promise<void>;
 
 export type PlayerRepository = {
-  get: Get;
-  create: Create;
-  getById: GetById;
-  update: Update;
-  delete: Delete;
+  get: GetPlayers;
+  create: CreatePlayer;
+  getById: GetPlayerById;
+  update: UpdatePlayer;
+  delete: DeletePlayer;
 };
