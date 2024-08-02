683a7509b329d6e09f2abfb63a466d2aa4479555
randriantsarafara-wbd <165807404+randriantsarafara-wbd@users.noreply.github.com>
2024-08-02 23:08:11 +0200

    feat: added new fields to domain entity and db schema

--- services/cosfa/src/domain/entities/player.entity.ts
+++ services/cosfa/src/domain/entities/player.entity.ts
@@ -2,9 +2,26 @@ import { BaseEntity } from '@packages/shared/src';
 
 export type CreatePlayerDTO = Partial<Player>;
 
+export type Address = {
+  streetNumber?: string;
+  neighborHood?: string;
+  district?: string;
+  postalCode?: string;
+  region?: string;
+  country?: string;
+};
+
+export type Contact = {
+  phoneNumbers?: string[];
+  emails?: string[];
+};
+
 export class Player extends BaseEntity<CreatePlayerDTO> {
   firstName: string;
   lastName: string;
+  birthDate: string | Date;
+  address?: Address;
+  contact?: Contact;
 
   constructor(options: CreatePlayerDTO) {
     super(options);
--- services/cosfa/src/infrastructure/schemas/player.schema.ts
+++ services/cosfa/src/infrastructure/schemas/player.schema.ts
@@ -1,9 +1,26 @@
 import { model, Schema } from 'mongoose';
 import { Player } from '../../domain/entities/player.entity';
 
+const AddressSchema = new Schema({
+  streetNumber: { type: String },
+  neighborHood: { type: String },
+  district: { type: String },
+  postalCode: { type: String },
+  region: { type: String },
+  country: { type: String },
+});
+
+const ContactSchema = new Schema({
+  phoneNumbers: { type: [String] },
+  emails: { type: [String] },
+});
+
 const PlayerSchema = new Schema({
-  firstName: { type: String },
-  lastName: { type: String },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  birthDate: { type: Date, required: true },
+  address: { type: AddressSchema },
+  contact: { type: ContactSchema },
 });
 
 export const PLAYER = 'Player';
