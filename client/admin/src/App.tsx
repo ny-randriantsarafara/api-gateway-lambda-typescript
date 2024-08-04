0400d96b1f8bbe6198cfe3df85bc1aef2ce455ed
randriantsarafara-wbd <165807404+randriantsarafara-wbd@users.noreply.github.com>
2024-08-04 19:22:09 +0200

    chore: enable caching

--- client/admin/src/App.tsx
+++ client/admin/src/App.tsx
@@ -1,4 +1,5 @@
 import { Admin, Resource } from 'react-admin';
+import { QueryClient } from '@tanstack/react-query';
 import { Layout } from './Layout';
 import { dataProvider } from './providers/data-provider.provider';
 import { PlayerCreate } from './modules/players/PlayerCreate';
@@ -9,9 +10,19 @@ import { CategoryEdit } from './modules/categories/CategoryEdit';
 import { CategoryCreate } from './modules/categories/CategoryCreate';
 import { CategoryShow } from './modules/categories/CategoryShow';
 
-export const App = () => (
-  <Admin layout={Layout} dataProvider={dataProvider(import.meta.env.VITE_API_BASE_URL)}>
-    <Resource name="categories" list={CategoryList} show={CategoryShow} create={CategoryCreate} edit={CategoryEdit} />
-    <Resource name="players" list={PlayerList} create={PlayerCreate} edit={PlayerEdit} />
-  </Admin>
-);
+export const App = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 60 * 1000,
+      },
+    },
+  });
+
+  return (
+    <Admin layout={Layout} dataProvider={dataProvider(import.meta.env.VITE_API_BASE_URL)} queryClient={queryClient}>
+      <Resource name="categories" list={CategoryList} show={CategoryShow} create={CategoryCreate} edit={CategoryEdit} />
+      <Resource name="players" list={PlayerList} create={PlayerCreate} edit={PlayerEdit} />
+    </Admin>
+  );
+};
